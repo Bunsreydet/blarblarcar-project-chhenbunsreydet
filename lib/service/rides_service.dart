7ae7fe306b36bc5fb1e942fcce744dc5f5b3a2ad
Bunsreydet Chhen <chhinbunsreydet@gmail.com>
2026-02-05 15:45:20 +0700

    BLA-001 - Rides Service - Filter rides methods

---
+++ lib/service/rides_service.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import '../dummy_data/dummy_data.dart';
+import '../model/ride/locations.dart';
+import '../model/ride/ride.dart';
+
+class RidesService {
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+  Location availableRides = 
+  //
+  //  filter the rides starting from given departure location
+  //
+  static List<Ride> _filterByDeparture(Location departure) {
+    Location departureLocation = 
+    return [];
+  }
+
+  //
+  //  filter the rides starting for the given requested seat number
+  //
+  static List<Ride> _filterBySeatRequested(int requestedSeat) {
+    return [];
+  }
+
+  //
+  //  filter the rides   with several optional criteria (flexible filter options)
+  //
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return [];
+  }
+}
