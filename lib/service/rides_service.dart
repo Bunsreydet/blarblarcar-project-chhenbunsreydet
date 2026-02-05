a2430bb33e3c50dc031df7a2ac1161874f00d133
Bunsreydet Chhen <chhinbunsreydet@gmail.com>
2026-02-05 16:06:17 +0700

    BLA-001 - Rides Services(correction)

--- lib/service/rides_service.dart
+++ lib/service/rides_service.dart
@@ -6,26 +6,41 @@ import '../model/ride/ride.dart';
 
 class RidesService {
   static List<Ride> availableRides = fakeRides; // TODO for now fake data
-  Location availableRides = 
   //
   //  filter the rides starting from given departure location
   //
   static List<Ride> _filterByDeparture(Location departure) {
-    Location departureLocation = 
-    return [];
+    //return availableRides.where((ride) => ride.departureLocation == departureLocation).toList();
+
+    List<Ride> result = [];
+    for (Ride ride in availableRides) {
+      if (ride.departureLocation == departure) {
+        result.add(ride);
+      }
+    }
   }
 
   //
   //  filter the rides starting for the given requested seat number
   //
   static List<Ride> _filterBySeatRequested(int requestedSeat) {
-    return [];
+    return availableRides
+        .where((ride) => ride.availableSeats >= requestedSeat)
+        .toList();
   }
 
   //
   //  filter the rides   with several optional criteria (flexible filter options)
   //
   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    return [];
+    List<Ride> results = availableRides;
+    if(departure != null){
+      results = _filterByDeparture(results, departure)
+      //results = results.where((rides) => ride.departureLocation >= seatRequested)
+    }
+    if(seatRequested != null){
+      results = _filterBySeatRequested(results, seatRequested);
+    }
+    return results;
   }
 }
