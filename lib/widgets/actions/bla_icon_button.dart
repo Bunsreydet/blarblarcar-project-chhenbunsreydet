dd8ff51a0934de2829aef5064fcdb803d2b7dc28
Bunsreydet Chhen <chhinbunsreydet@gmail.com>
2026-02-21 22:51:43 +0700

    BLA-002-Implement Button

---
+++ lib/widgets/actions/bla_icon_button.dart
@@ -0,0 +1,19 @@
+import 'package:blarblarcar/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class BlaIconButton extends StatelessWidget {
+  final IconData? icon;
+  final VoidCallback? onPressed;
+  const BlaIconButton({super.key, required this.icon, required this.onPressed });
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Icon(icon, size: BlaSize.icon, color: BlaColors.primary),
+      ),
+    );
+  }
+}
